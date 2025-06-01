38bb34aed0e4925a0acab3dae4c62208226373a5
zahra-saif <zsaif7223@gmail.com>
2025-06-01 13:41:13 +0500

    Update calcode.py

--- calcode.py
+++ calcode.py
@@ -1,4 +1,27 @@
-def add(a, b):
+print("\nChoose an operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ").strip()
+
+    if choice not in ['1', '2', '3', '4']:
+        print("Invalid choice. Exiting.")
+        return
+
+    if choice == '1':
+        operation = '+'
+        func = add
+    elif choice == '2':
+        operation = '-'
+        func = subtract
+    elif choice == '3':
+        operation = '*'
+        func = multiply
+    elif choice == '4':
+        operation = '/'
+        func = dividedef add(a, b):
     return a + b
 def subtract(a, b):
     return a - b
