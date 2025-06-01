bb7511520bc51d8b5f3d0b938581ed2fd969837a
ZOHA RASHID <zoha.rashid.151@gmail.com>
2025-06-01 13:32:25 +0500

    Create calcode.py

---
+++ calcode.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        print("Error: Cannot divide by zero.")
+        return None
+    return a / b
