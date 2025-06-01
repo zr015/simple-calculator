7ffa18cda2ab9550d7dd55dd9cec237a67cc91eb
ZOHA RASHID <zoha.rashid.151@gmail.com>
2025-06-01 13:49:33 +0500

    Update calcode.py

--- calcode.py
+++ calcode.py
@@ -1,3 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        print("Error: Cannot divide by zero.")
+        return None
+    return a / b
 def log_operation(operation, a, b, result):
     with open("log.txt", "a") as file:
         file.write(f"{a} {operation} {b} = {result}\n")
